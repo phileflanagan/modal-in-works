b7397cb3b7213863c184e1535bd2dfa175bcd69a
Phil Flanagan <phileflanagan@gmail.com>
2017-01-22 16:42:00 -0500

    update forms

--- form-login-updated.php
+++ form-login-updated.php
@@ -4,19 +4,19 @@ $setting = SwpmSettings::get_instance();
 $password_reset_url = $setting->get_value('reset-page-url');
 $join_url = $setting->get_value('join-us-page-url');
 ?>
-<div class="swpm-login-widget-form">
+<div class="lec-form">
     <form id="swpm-login-form" name="swpm-login-form" method="post" action="">
         <div class="swpm-login-form-inner">
             <label for="swpm_user_name" class="swpm-label"><?php echo SwpmUtils::_('Username or Email') ?></label>
             <input type="text" class="swpm-text-field swpm-username-field" id="swpm_user_name" value="" size="25" name="swpm_user_name" />
 
             <label for="swpm_password" class="swpm-label"><?php echo SwpmUtils::_('Password') ?></label>
-            <input type="password" class="swpm-text-field swpm-password-field" id="swpm_password" value="" size="25" name="swpm_password" />    
+            <input type="password" class="swpm-text-field swpm-password-field" id="swpm_password" value="" size="25" name="swpm_password" />
 
             <div class="swpm-before-login-submit-section"><?php echo apply_filters('swpm_before_login_form_submit_button', ''); ?></div>
 
             <div class="lec-login-submit">
-                <input type="submit" class="swpm-login-form-submit" name="swpm-login" value="<?php echo SwpmUtils::_('Login') ?>"/>
+                <input type="submit" class="form-button-gray" name="swpm-login" value="<?php echo SwpmUtils::_('Login') ?>"/>
                 <div class="lec-remember-me">
                   <span class="swpm-remember-checkbox"><input type="checkbox" name="rememberme" value="checked='checked'"></span>
                   <span class="swpm-rember-label"> <?php echo SwpmUtils::_('Remember Me') ?></span>
@@ -24,6 +24,10 @@ $join_url = $setting->get_value('join-us-page-url');
                 <div class="swpm-login-action-msg">
                   <span class="swpm-login-widget-action-msg"><?php echo $auth->get_message(); ?></span>
                 </div>
+          </div>
+
+          <div class="lec-forgot-pass-link">
+              <a id="forgot_pass" class="swpm-login-form-pw-reset-link"  href="<?php echo $password_reset_url; ?>"><?php echo SwpmUtils::_('Lost your password') ?>?</a>
           </div>
     </form>
 </div>
--- form-updated.php
+++ form-updated.php
@@ -1,27 +1,26 @@
-<div class="swpm-registration-widget-form">
+<div class="lec-form">
     <form id="swpm-registration-form" name="swpm-registration-form" method="post" action="">
         <input type ="hidden" name="level_identifier" value="<?php echo $level_identifier ?>" />
-             <label for="user_name"><?php echo SwpmUtils::_('Username') ?></label>
+             <label for="user_name"><?php echo SwpmUtils::_('Username / Company*') ?></label>
              <input type="text" id="user_name" class="validate[required,custom[noapostrophe],custom[SWPMUserName],minSize[4],ajax[ajaxUserCall]]" value="<?php echo esc_attr($user_name); ?>" size="50" name="user_name" />
 
-            <label for="email"><?php echo SwpmUtils::_('Email') ?></label></td>
+            <label for="email"><?php echo SwpmUtils::_('Email*') ?></label></td>
             <input type="text" id="email" class="validate[required,custom[email],ajax[ajaxEmailCall]]" value="<?php echo esc_attr($email); ?>" size="50" name="email" />
 
 
-            <label for="password"><?php echo SwpmUtils::_('Password') ?></label>
+            <label for="password"><?php echo SwpmUtils::_('Password*') ?></label>
             <input type="password" autocomplete="off" id="password" value="" size="50" name="password" />
 
-            <label for="password_re"><?php echo SwpmUtils::_('Repeat Password') ?></label>
+            <label for="password_re"><?php echo SwpmUtils::_('Repeat Password*') ?></label>
             <input type="password" autocomplete="off" id="password_re" value="" size="50" name="password_re" />
 
-            <label for="first_name"><?php echo SwpmUtils::_('First Name') ?></label>
+            <label for="first_name"><?php echo SwpmUtils::_('First Name*') ?></label>
             <input type="text" id="first_name" value="<?php echo esc_attr($first_name); ?>" size="50" name="first_name" />
 
-            <label for="last_name"><?php echo SwpmUtils::_('Last Name') ?></label>
+            <label for="last_name"><?php echo SwpmUtils::_('Last Name*') ?></label>
             <input type="text" id="last_name" value="<?php echo esc_attr($last_name); ?>" size="50" name="last_name" />
 
                     <?php
-                    echo $membership_level_alias;//Show the level name in the form.
                     //Add the input fields for the level data.
                     echo '<input type="hidden" value="'.$membership_level.'" size="50" name="membership_level" id="membership_level" />';
                     //Add the level input verification data.
@@ -36,8 +35,8 @@
 
         <div class="swpm-before-registration-submit-section" align="center"><?php echo apply_filters('swpm_before_registration_submit_button', ''); ?></div>
 
-        <div class="swpm-registration-submit-section" align="center">
-            <input type="submit" value="<?php echo SwpmUtils::_('Register') ?>" class="swpm-registration-submit" name="swpm_registration_submit" />
+        <div class="swpm-registration-submit-section" align="left">
+            <input class="form-button-gray" type="submit" value="<?php echo SwpmUtils::_('Register') ?>" class="swpm-registration-submit" name="swpm_registration_submit" />
         </div>
 
         <input type="hidden" name="action" value="custom_posts" />
